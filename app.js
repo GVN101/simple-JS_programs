92567d4a4bd7bf7eac173e8e70e6610672a00a5a
GOVIND V NAMBOOTHIRI <gvndnamboothiri@gmail.com>
2024-09-01 19:27:39 +0530

    minor change

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ let temp;
 
 function convert(value){
     if(toFahrenheit.checked){
-        temp = (textBox.value * 9/5) + 32;
-        result.textContent = `${textBox.value}°C is ${temp.toFixed(2)}°F`;
+        temp = (Number(textBox.value) * 9/5) + 32;
+        result.textContent = `${Number(textBox.value)}°C is ${temp.toFixed(2)}°F`;
     }
     else if(toCelcius.checked){
-        temp = (textBox.value - 32) * 5/9;
-        result.textContent = `${textBox.value}°F is ${temp.toFixed(2)}°C`;
+        temp = (Number(textBox.value) - 32) * 5/9;
+        result.textContent = `${Number(textBox.value)}°F is ${temp.toFixed(2)}°C`;
     }
     else{
         result.textContent = "Please select a conversion type";
